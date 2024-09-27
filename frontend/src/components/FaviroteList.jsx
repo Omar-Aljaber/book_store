4a12123c4f94e6ed12ffdb4af79a193b502b6d0a
Omar.Aljaber <omar.aljaber@proplant-mvi.com>
2024-09-27 11:18:45 +0200

    expanded the faviroteList

--- frontend/src/components/FaviroteList.jsx
+++ frontend/src/components/FaviroteList.jsx
@@ -1,18 +1,32 @@
-import React from "react";
-import redArrow from "../style/imgs/red-arrow.png";
-import greenArrow from "../style/imgs/green-arrow.png";
+import React, { useState } from "react";
 import Delete from "../style/imgs/delete.ico";
 import cart from "../style/imgs/cart.png";
-import star from "../style/imgs/star.ico";
 import defaultPhoto from "../style/imgs/book.png";
-import data from "../data/homeBooks.json";
-import { TITLES, BUTTONS_TITLE, FAVIROTE_LIST } from "../constants/Language_de";
+import reviewStar from "../style/imgs/star.ico";
+import defaultStar from "../style/imgs/default-star.ico";
+import helper from "../service/helper";
+import data from "../data/faviroteBooks.json";
+import Message from "./Message";
+import { TITLES, BUTTONS_TITLE, FAVIROTE_LIST, MESSAGES } from "../constants/Language_de";
 
 export default function FaviroteList(props) {
 
+    const [message, setMessage] = useState("");
+    const [messageType, setMessageType] = useState("");
+
+    const addToCart = () => {
+        setMessage(MESSAGES.TEXT.ADDED_TO_CART);
+        setMessageType(MESSAGES.TYPE.CONFIRM);
+    };
+
+    const deleteBook = () => {
+        setMessage(MESSAGES.TEXT.DELETED_FROM_LIST);
+        setMessageType(MESSAGES.TYPE.REMOVED);
+    };
+
     const bookPart = (data, category) => {
         return (
-            data.map((book, index) => {
+            data.map((book, index) => { 
                 const bookPhoto = book.description.photo != null ? book.description.photo : defaultPhoto;
                 return (
                     <div key={index} className="book-section">
@@ -21,24 +35,16 @@ export default function FaviroteList(props) {
                             <div className="title-review">
                                 <h4 className="book-title">{book.title}</h4>
                                 <div className="review"> 
-                                    <img src={star} width={15} />
-                                    <img src={star} width={15} />
-                                    <img src={star} width={15} />
-                                    <img src={star} width={15} />
-                                    <img src={star} width={15} />
-                                    <div>325 reviews</div>
+                                    {helper.reviewStars(book.review.stars, reviewStar, defaultStar)}
+                                    <div className="reviews">
+                                        {book.review.count} reviews
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <h4 className="quantity">
-                            <img src={redArrow} width={25} height={25}/>
-                            <h3>5</h3>
-                            <img src={greenArrow} width={25} height={25}/>
-                        </h4>
                         <h2 className="price">{book.description.price}$</h2>
-                        <h2 className="total">{book.description.price}$</h2>
-                        <img src={cart} width={25} height={25}/>
-                        <img src={Delete} width={25} height={25}/>
+                        <img className="cartIcon" src={cart} onClick={addToCart} width={25} height={25}/>
+                        <img className="deleteIcon" src={Delete} onClick={deleteBook} width={25} height={25}/>
                     </div>
                 )
             })
@@ -57,27 +63,29 @@ export default function FaviroteList(props) {
     };
 
     return (
-        <div className="favorite-list">
-            <form>
-                <div className="title">
-                    <div>{TITLES.MY_VAVIROTE}</div>
-                </div>
-                <div className="list-info">
-                    <div className="column-title">
-                        <div className="project">{FAVIROTE_LIST.PROJECT}</div>
-                        <div className="quantity">{FAVIROTE_LIST.QUANTITY}</div>
-                        <div className="price">{FAVIROTE_LIST.PRICE}</div>
-                        <div>{FAVIROTE_LIST.TOTAL}</div>
-                        <div>{FAVIROTE_LIST.ADD_TO_CART}</div>
-                        <div>{FAVIROTE_LIST.DELETE}</div>
+        <div>
+            {message && <Message type={messageType} text={message} setMessage={setMessage} />}
+            <div className="favorite-list">
+                <form>
+                    <div className="title">
+                        <div>{TITLES.MY_VAVIROTE}</div>
                     </div>
-                    {bookPart(data.books)}
-                </div>
-                <div>
-                    <button onClick={(e) => {goToCart(e)}}>{BUTTONS_TITLE.GO_TO_CART}</button>
-                    <button className="shopping" onClick={(e) => {continueShopping(e)}}>{BUTTONS_TITLE.SHOPPING}</button>
-                </div>
-            </form>
+                    <div className="list-info">
+                        <div className="column-title">
+                            <div className="project">{FAVIROTE_LIST.PROJECT}</div>
+                            <div className="price">{FAVIROTE_LIST.PRICE}</div>
+                            <div>{FAVIROTE_LIST.ADD_TO_CART}</div>
+                            <div>{FAVIROTE_LIST.DELETE}</div>
+                        </div>
+                        {bookPart(data.books)}
+                    </div>
+                    <div>
+                        <button onClick={(e) => {goToCart(e)}}>{BUTTONS_TITLE.GO_TO_CART}</button>
+                        <button className="shopping" onClick={(e) => {continueShopping(e)}}>{BUTTONS_TITLE.SHOPPING}</button>
+                    </div>
+                </form>
+            </div>
         </div>
+            
     );
 }

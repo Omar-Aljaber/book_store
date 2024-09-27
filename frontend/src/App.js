9e7015f357c5163f852db6c56b7e3d3e6551b80d
Omar.Aljaber <omar.aljaber@proplant-mvi.com>
2024-09-27 11:17:10 +0200

    added a message for mobile version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./views/Home";
 import Books from "./views/Books";
+import Contact from "./views/Contact";
 import "./style/index.scss";
 
 function App() {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Checking if the device is a mobile phone or tablet (screen width < 1500px)
+  useEffect(() => {
+    const checkIsMobile = () => {
+        if (window.innerWidth < 1500) {
+            setIsMobile(true);
+        } else {
+            setIsMobile(false);
+        }
+    };
+
+    checkIsMobile();
+
+    // Add an event listener to respond to window size changes
+    window.addEventListener('resize', checkIsMobile);
+
+    // Clean up the event listener when the component is unloaded
+      return () => window.removeEventListener('resize', checkIsMobile);
+  }, []);
+
+  if (isMobile) {
+      return (
+          <div className="mobile-message">
+              <h1>Mobile version not available yet</h1>
+              <p>The version for mobile phones and tablets is still in development. Please visit the site on a desktop.</p>
+          </div>
+      );
+  }
+
   return (
     <BrowserRouter>
       <Switch>
@@ -13,6 +45,9 @@ function App() {
         </Route>
         <Route path="/books/category=:category&publisher=:publisher">
           <Books />
+        </Route>
+        <Route path="/contact">
+          <Contact />
         </Route>
       </Switch>
     </BrowserRouter>

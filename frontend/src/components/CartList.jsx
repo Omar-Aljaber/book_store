8fa1ab059a3b8cd2f8471019064e102477187535
Omar.Aljaber <omar.aljaber@proplant-mvi.com>
2024-09-27 11:18:28 +0200

    expanded the cartlist

--- frontend/src/components/CartList.jsx
+++ frontend/src/components/CartList.jsx
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import Delete from "../style/imgs/delete.ico";
+import reviewStar from "../style/imgs/star.ico";
+import defaultStar from "../style/imgs/default-star.ico";
+import helper from "../service/helper";
+import defaultPhoto from "../style/imgs/book.png";
 import redArrow from "../style/imgs/red-arrow.png";
 import greenArrow from "../style/imgs/green-arrow.png";
-import Delete from "../style/imgs/delete.ico";
-import star from "../style/imgs/star.ico";
-import defaultPhoto from "../style/imgs/book.png";
-import data from "../data/homeBooks.json";
-import { TITLES, BUTTONS_TITLE, FAVIROTE_LIST } from "../constants/Language_de";
+import data from "../data/cartListBooks.json";
+import Message from "./Message";
+import { TITLES, BUTTONS_TITLE, FAVIROTE_LIST, MESSAGES } from "../constants/Language_de";
 
 export default function CartList(props) {
     
+    const [message, setMessage] = useState("");
+    const [messageType, setMessageType] = useState("");
+    const [bookCounts, setBookCounts] = useState(data.books.map(() => 1));
+
+    const onReduceBook = (e, index) => {
+        setBookCounts(prevCounts => {
+            const updatedCounts = [...prevCounts];
+            if (updatedCounts[index] > 1) {
+                updatedCounts[index]--;
+            }
+            return updatedCounts;
+        });
+    };
+    
+    const onAddBook = (e, index) => {
+        setBookCounts(prevCounts => {
+            const updatedCounts = [...prevCounts];
+            updatedCounts[index]++;
+            return updatedCounts;
+        });
+    };
+    
+
+    const deleteBook = () => {
+        setMessage(MESSAGES.TEXT.DELETED_FROM_LIST);
+        setMessageType(MESSAGES.TYPE.REMOVED);
+    };
+
     const bookPart = (data, category) => {
         return (
             data.map((book, index) => {
@@ -20,27 +51,25 @@ export default function CartList(props) {
                             <div className="title-review">
                                 <h4 className="book-title">{book.title}</h4>
                                 <div className="review"> 
-                                    <img src={star} width={15} />
-                                    <img src={star} width={15} />
-                                    <img src={star} width={15} />
-                                    <img src={star} width={15} />
-                                    <img src={star} width={15} />
-                                    <div>325 reviews</div>
+                                    {helper.reviewStars(book.review.stars, reviewStar, defaultStar)}
+                                    <div className="reviews">
+                                        {book.review.count} reviews
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <h4 className="quantity">
-                            <img src={redArrow} width={25} height={25}/>
-                            <h3>5</h3>
-                            <img src={greenArrow} width={25} height={25}/>
+                            <img name={book.title} src={redArrow} onClick={(e) => onReduceBook(e, index)} width={25} height={25}/>
+                            <h3>{bookCounts[index]}</h3>
+                            <img name={index} src={greenArrow} onClick={(e) => onAddBook(e, index)} width={25} height={25}/>
                         </h4>
                         <h2 className="price">{book.description.price}$</h2>
-                        <h2 className="total">{book.description.price}$</h2>
-                        <img src={Delete} width={25} height={25}/>
+                        <h2 className="total">{book.description.price * bookCounts[index]}$</h2>
+                        <img src={Delete} onClick={deleteBook} width={25} height={25}/>
                     </div>
-                )
+                );
             })
-        )
+        );
     };
 
     const goToBayment = (e) => {
@@ -54,32 +83,29 @@ export default function CartList(props) {
     };
 
     return (
-        <div className="cart-list">
-            <form>
-                <div className="title">
-                    <div>{TITLES.SHOPPING_LIST}</div>
-                </div>
-                <div className="list-info">
-                    <div className="column-title">
-                        <div className="project">{FAVIROTE_LIST.PROJECT}</div>
-                        <div className="quantity">{FAVIROTE_LIST.QUANTITY}</div>
-                        <div className="price">{FAVIROTE_LIST.PRICE}</div>
-                        <div>{FAVIROTE_LIST.TOTAL}</div>
-                        <div>{FAVIROTE_LIST.DELETE}</div>
+        <div>
+            {message && <Message type={messageType} text={message} setMessage={setMessage} />}
+            <div className="cart-list">
+                <form>
+                    <div className="title">
+                        <div>{TITLES.SHOPPING_LIST}</div>
                     </div>
-                    {bookPart(data.books)}
-                </div>
-                <div className="summe">Summe: 250 $</div>
-                <div className="footer">
-                    <button
-                        onClick={(e) => {goToBayment(e)}}
-                    >{BUTTONS_TITLE.PAYMENT}</button>
-                    <button 
-                        className="shopping" 
-                        onClick={(e) => {continueShopping(e)}}
-                    >{BUTTONS_TITLE.SHOPPING}</button>
-                </div>
-            </form>
+                    <div className="list-info">
+                        <div className="column-title">
+                            <div className="project">{FAVIROTE_LIST.PROJECT}</div>
+                            <div className="quantity">{FAVIROTE_LIST.QUANTITY}</div>
+                            <div className="price">{FAVIROTE_LIST.PRICE}</div>
+                            <div>{FAVIROTE_LIST.TOTAL}</div>
+                            <div>{FAVIROTE_LIST.DELETE}</div>
+                        </div>
+                        {bookPart(data.books)}
+                    </div>
+                    <div>
+                        <button onClick={(e) => {goToBayment(e)}}>{BUTTONS_TITLE.PAYMENT}</button>
+                        <button className="shopping" onClick={(e) => {continueShopping(e)}}>{BUTTONS_TITLE.SHOPPING}</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
